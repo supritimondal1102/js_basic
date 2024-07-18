d5af94cfd4d30ec96e7d2cb44e54d916c8ea89d2
Supriti Mondal <165676092+supritimondal1102@users.noreply.github.com>
2024-07-18 18:19:40 +0530

    Add files via upload

---
+++ unary_ope.js
@@ -0,0 +1,15 @@
+//unary Operator
+let a = 5;
+let b = 2;
+
+console.log("a = ", a, " & b = ", b)
+//a = a-1;
+//a--;
+console.log("++a = ", ++a);
+console.log("a = ", a);
+
+console.log("a++ = ", a++); //first line dont chahge the value and  next line chenge the value. //5
+console.log("a = ", a); // a = 6
+
+console.log("--a = ", --a);
+console.log("a = ", a);
