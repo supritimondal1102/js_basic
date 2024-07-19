0748e02f20434e4b0cb793bcbe4631a8b0b65a30
Supriti Mondal <165676092+supritimondal1102@users.noreply.github.com>
2024-07-20 01:25:15 +0530

    Add files via upload

---
+++ else_if.js
@@ -0,0 +1,15 @@
+let mode = "blue";
+
+let color;
+
+if(mode === "dark") {
+    color = "black";
+} else if (mode === "blue") {
+    color = "blue";
+}else if (mode === "pink") {
+    color = "pink";
+}else {
+    color = "white";
+}
+
+console.log(color);
