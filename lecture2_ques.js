b3c485e2fc99f754bba001eebc4e249b75139509
Supriti Mondal <165676092+supritimondal1102@users.noreply.github.com>
2024-07-21 01:45:28 +0530

    Add files via upload

---
+++ lecture2_ques.js
@@ -0,0 +1,25 @@
+//QS1
+//let num = prompt("enter a number:");
+
+//if (num % 5 === 0) {
+    //console.log(num, "is a multiple of 5");
+//}else{
+  //  console.log(num, "is NOT a multiple of 5");
+//}
+
+//QS2
+
+let score = 99;
+let grade;
+
+if (score >= 90 && score <= 100){
+    console.log("grade A");
+} else if (score >= 70 && score <= 89){
+    console.log("grade B");
+} else if (score >= 60 && score <= 69){
+    console.log("grade C");
+} else if (score >= 50 && score <= 59){
+    console.log("greade D");
+} else if (score >= 0 && score <= 49){
+     console.log("grade F");
+}
