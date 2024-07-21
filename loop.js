41179afac2e98120b50cfb728c21cacee94b1578
Supriti Mondal <165676092+supritimondal1102@users.noreply.github.com>
2024-07-22 01:45:02 +0530

    Add files via upload

---
+++ loop.js
@@ -0,0 +1,14 @@
+//for loop
+//print 1 to 5
+
+for(let count=1; count<=5; count++) {
+    console.log("supriti")
+}
+
+//calculate sum of first n numbers
+
+let sum = 0;
+for(let i=1; i=5; i++) {
+    sum = sum +1;
+}
+console.log("sum = ", sum);
