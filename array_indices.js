197bc620e717c874242ff26532370849ad09a95c
Supriti Mondal <165676092+supritimondal1102@users.noreply.github.com>
2024-07-30 22:44:24 +0530

    Add files via upload

---
+++ array_indices.js
@@ -0,0 +1,4 @@
+let marks = [97, 89, 45, 74, 39];
+console.log(marks[5]);
+console.log(marks[0] = 66);
+console.log("marks");
---
+++ arrays.js
@@ -0,0 +1,6 @@
+let marks = [97, 89, 45, 74, 39];
+console.log(marks);
+console.log(marks.length); //property
+
+let heroes = ["ironman", "thor", "hulk", "spiderman"];
+console.log(heroes);
