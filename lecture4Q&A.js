a98e520bbded4d4886b1863802b99578e6376130
Supriti Mondal <165676092+supritimondal1102@users.noreply.github.com>
2024-07-31 22:43:28 +0530

    Add files via upload

---
+++ lecture4Q&A.js
@@ -0,0 +1,12 @@
+//qs1 for a given array with marks of students -> [85,97,44,37,76,60] Find the average marks of the entire class.
+let marks = [85, 97, 44, 37, 76, 60];
+
+let sum = 0;
+
+for (let val of marks) {
+    //console.log(val); //for print value
+    sum += val;
+}
+
+let avg = sum / marks.length;
+console.log(`avg marks of the class = ${avg}`);
