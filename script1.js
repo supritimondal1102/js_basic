960621ae7aead72f34f30f20d479f67121fa0a83
Supriti Mondal <165676092+supritimondal1102@users.noreply.github.com>
2024-08-04 22:40:35 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,8 @@
+let foodItems = ["potato", "apple", "litchi", "tomato"];
+
+foodItems.push("chips", "burger", "paneer");
+
+console.log(foodItems);
+let deleteditem = foodItems.pop();
+console.log(foodItems);
+console.log("deleted");
---
+++ tostring.js
@@ -0,0 +1,6 @@
+let foodItems = ["potato", "apple", "litchi", "tomato"];
+console.log(foodItems);
+console.log(foodItems.toString());
+
+let marks =[97, 87, 95, 67];
+console.log(marks.toString());
